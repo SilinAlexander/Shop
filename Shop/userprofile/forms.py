72c075183c4d76ce2554e732c6cfb43d992dcbfe
SilinAlexander <asilin1997@mail.ru>
2021-02-07 12:17:09 +0500

    4

--- Shop/userprofile/forms.py
+++ Shop/userprofile/forms.py
@@ -4,8 +4,24 @@ from django_countries.widgets import CountrySelectWidget
 
 
 class UserAddressForm(forms.ModelForm):
+
     city = forms.CharField(widget=forms.TextInput({
         'placeholder': 'city',
+        'class': 'form-control'
+    }))
+
+    region = forms.CharField(widget=forms.TextInput({
+        'placeholder': 'region',
+        'class': 'form-control'
+    }))
+
+    street = forms.CharField(widget=forms.TextInput({
+        'placeholder': 'street',
+        'class': 'form-control'
+    }))
+
+    index = forms.CharField(widget=forms.TextInput({
+        'placeholder': 'index',
         'class': 'form-control'
     }))
 
